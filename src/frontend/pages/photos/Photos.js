c0d654d85fd69b5608f0abcd33efd6cb0fdd0349
Tom Young <tom.young@bp.com>
2021-10-19 16:31:24 +0100

    Added photos page(under construction)

---
+++ src/frontend/pages/photos/Photos.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import "./Photos.css";
+
+export default function Photos() {
+
+    return (
+        <>
+            <title>Mars Rover App</title>
+            <main className="photos-page">
+                <h1>Mars Rover Photos</h1>
+                <h2>This page is currenlty under construction, come back soon!</h2>
+
+            </main>
+        </>
+    );
+}
+ 
