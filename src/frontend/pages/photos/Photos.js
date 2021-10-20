3707c0396d4f393c22d5eb43b82ec47e26b14020
Tom Young <tom.young@bp.com>
2021-10-20 15:51:16 +0100

    Nav Bar works and code formatted

--- src/frontend/pages/photos/Photos.js
+++ src/frontend/pages/photos/Photos.js
@@ -4,21 +4,17 @@ import "./Photos.css";
 
 const logo = "/mars-rover-logo3.png";
 
-
 export default function Photos() {
-
     return (
         <>
-        <title>Mars Rover App</title>
-        <main className="photos-page">
-          <body>
-              <NavBar/>
-            <h1>Mars Rover Photos</h1>
-            <h2>This page is currently under construction, come back soon!</h2>
-          </body>
-        </main>
-      </>
-
+            <title>Mars Rover App</title>
+            <main className="photos-page">
+                <body>
+                    <NavBar />
+                    <h1>Mars Rover Photos</h1>
+                    <h2>This page is currently under construction, come back soon!</h2>
+                </body>
+            </main>
+        </>
     );
 }
- 
