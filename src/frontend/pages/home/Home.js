bdf56e2240b17f2f49824a7bb3639d0eda54ed00
Tom Young <tom.young@bp.com>
2021-10-22 11:30:53 +0100

    updated format of page

--- src/frontend/pages/home/Home.js
+++ src/frontend/pages/home/Home.js
@@ -21,48 +21,69 @@ export default function Home() {
                     <h2>Quotation of the day</h2>
                     <p className="quotation">{quotation?.text}</p>
                     <p className="attribution">—{quotation?.attribution}</p>
-                    <div className ="Rovers">
-                        <h3>Curiosity Rover</h3>
-                        <img className = "curiosityImg" src = {curiosity} alt = "Curisosity Rover" width= "241.3px" height ="187.6px"/>
-                            <p1>
+
+                    <div class="container">
+                        <div class="image">
+                            <img src={curiosity} alt="Curisosity Rover" width="241.3px" height="187.6px" />
+                        </div>
+                        <div class="text">
                             Curiosity is the largest and most capable rover ever sent to Mars. Curiosity is a car-sized Mars rover designed to explore
                             the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission. Curiosity was launched from Cape
                             Canaveral (CCAFS) on 26 November 2011, at 15:02:00 UTC and landed on Aeolis Palus inside Gale crater on Mars on 6 August
-                            2012, 05:17:57 UTC.<br/>
-                            It is exploring Gale Crater, which is near the
-                            equator of Mars. The rover uses a nuclear power to operate. The MSL mission has
-                            four main scientific goals: study Martian climate and geology, search for water, and find out whether Mars could have ever
-                            supported life. Curiosity carries the most advanced scientific equipment ever used on the surface of Mars.
-                        </p1>
-                        
+                            2012, 05:17:57 UTC.
+                            <br />
+                            It is exploring Gale Crater, which is near the equator of Mars. The rover uses a nuclear power to operate. The MSL mission
+                            has four main scientific goals: study Martian climate and geology, search for water, and find out whether Mars could have
+                            ever supported life. Curiosity carries the most advanced scientific equipment ever used on the surface of Mars.
+                        </div>
                     </div>
-                    <div className ="Rovers">
-                        <br/><h3>Opportunity Rover</h3>
-                        <img className = "oppotunityImg" src = {opportunity} alt = "Oppotunity Rover" width= "241.3px" height ="187.6px"/>
+                    <div className="flex-container">
+                        <div>
+                            <h3>Curiosity Rover</h3>
+                            <img src={curiosity} alt="Curisosity Rover" width="241.3px" height="187.6px" />
                             <p1>
-                            Curiosity is the largest and most capable rover ever sent to Mars. Curiosity is a car-sized Mars rover designed to explore
-                            the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission. Curiosity was launched from Cape
-                            Canaveral (CCAFS) on 26 November 2011, at 15:02:00 UTC and landed on Aeolis Palus inside Gale crater on Mars on 6 August
-                            2012, 05:17:57 UTC.<br/>
-                            It is exploring Gale Crater, which is near the
-                            equator of Mars. The rover uses a nuclear power to operate. The MSL mission has
-                            four main scientific goals: study Martian climate and geology, search for water, and find out whether Mars could have ever
-                            supported life. Curiosity carries the most advanced scientific equipment ever used on the surface of Mars.
-                        </p1>
-                    </div>
-                    <div className ="Rovers">
-                        <h3>Spirit Rover</h3>
-                        <img className = "spiritImg" src = {spirit} alt = "Spirit Rover" width= "241.3px" height ="187.6px"/>
+                                Curiosity is the largest and most capable rover ever sent to Mars. Curiosity is a car-sized Mars rover designed to
+                                explore the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission. Curiosity was launched from
+                                Cape Canaveral (CCAFS) on 26 November 2011, at 15:02:00 UTC and landed on Aeolis Palus inside Gale crater on Mars on 6
+                                August 2012, 05:17:57 UTC.
+                                <br />
+                                It is exploring Gale Crater, which is near the equator of Mars. The rover uses a nuclear power to operate. The MSL
+                                mission has four main scientific goals: study Martian climate and geology, search for water, and find out whether Mars
+                                could have ever supported life. Curiosity carries the most advanced scientific equipment ever used on the surface of
+                                Mars.
+                            </p1>
+                        </div>
+                        <div>
+                            <br />
+                            <h3>Opportunity Rover</h3>
+                            <img className="oppotunityImg" src={opportunity} alt="Oppotunity Rover" width="241.3px" height="187.6px" />
                             <p1>
-                            Curiosity is the largest and most capable rover ever sent to Mars. Curiosity is a car-sized Mars rover designed to explore
-                            the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission. Curiosity was launched from Cape
-                            Canaveral (CCAFS) on 26 November 2011, at 15:02:00 UTC and landed on Aeolis Palus inside Gale crater on Mars on 6 August
-                            2012, 05:17:57 UTC.<br/>
-                            It is exploring Gale Crater, which is near the
-                            equator of Mars. The rover uses a nuclear power to operate. The MSL mission has
-                            four main scientific goals: study Martian climate and geology, search for water, and find out whether Mars could have ever
-                            supported life. Curiosity carries the most advanced scientific equipment ever used on the surface of Mars.
-                        </p1>
+                                Curiosity is the largest and most capable rover ever sent to Mars. Curiosity is a car-sized Mars rover designed to
+                                explore the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission. Curiosity was launched from
+                                Cape Canaveral (CCAFS) on 26 November 2011, at 15:02:00 UTC and landed on Aeolis Palus inside Gale crater on Mars on 6
+                                August 2012, 05:17:57 UTC.
+                                <br />
+                                It is exploring Gale Crater, which is near the equator of Mars. The rover uses a nuclear power to operate. The MSL
+                                mission has four main scientific goals: study Martian climate and geology, search for water, and find out whether Mars
+                                could have ever supported life. Curiosity carries the most advanced scientific equipment ever used on the surface of
+                                Mars.
+                            </p1>
+                        </div>
+                        <div>
+                            <h3>Spirit Rover</h3>
+                            <img className="spiritImg" src={spirit} alt="Spirit Rover" width="241.3px" height="187.6px" />
+                            <p1>
+                                Curiosity is the largest and most capable rover ever sent to Mars. Curiosity is a car-sized Mars rover designed to
+                                explore the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission. Curiosity was launched from
+                                Cape Canaveral (CCAFS) on 26 November 2011, at 15:02:00 UTC and landed on Aeolis Palus inside Gale crater on Mars on 6
+                                August 2012, 05:17:57 UTC.
+                                <br />
+                                It is exploring Gale Crater, which is near the equator of Mars. The rover uses a nuclear power to operate. The MSL
+                                mission has four main scientific goals: study Martian climate and geology, search for water, and find out whether Mars
+                                could have ever supported life. Curiosity carries the most advanced scientific equipment ever used on the surface of
+                                Mars.
+                            </p1>
+                        </div>
                     </div>
                 </body>
             </main>
