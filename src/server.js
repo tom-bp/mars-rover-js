211050c27873fdcf3768d185bb925649364533c6
Tom Young <tom.young@bp.com>
2021-10-25 16:39:15 +0100

    setup complete needs testing

--- src/server.js
+++ src/server.js
@@ -2,11 +2,34 @@ import express from "express";
 import { urlencoded, json } from "body-parser";
 import apiRoutes from "./backend/apiRoutes.js";
 import renderingRoutes from "./backend/renderingRoutes.js";
+import passport from "passport";
+import passportJwt from "passport-jwt";
 
-const passport = import("passport");
-const passportJwt = import("passport-jwt");
-
+const options = {
+    jwtFromRequest: passportJwt.ExtractJwt.fromHeader("x-access-token"),
+    secretOrKey: "secret",
+};
+passport.use(
+    new passportJwt.Strategy(options, function (decodedJwt, next) {
+        const username = decodedJwt.username;
+        db.any("SELECT * FROM accounts ")
+            .then(function (data) {
+                const account = data.find((account) => account.name === username);
+                if (account !== undefined) {
+                    next(null, account);
+                } else {
+                    return next(null, false);
+                }
+            })
+            .catch(function (error) {
+                next(error, false);
+                console.error(error);
+            });
+    })
+);
 const app = express();
+
+app.use(passport.initialize());
 app.disable("x-powered-by");
 app.use(urlencoded({ extended: false }));
 app.use(json());
@@ -15,29 +38,3 @@ app.use(express.static(process.env.RAZZLE_PUBLIC_DIR));
 app.use("/", renderingRoutes);
 
 export default app;
-
-
-const options = {
-    jwtFromRequest: passportJwt.ExtractJwt.fromHeader("x-access-token"),
-    secretOrKey: "secret",
-  };
-  
-  passport.use(
-    new passportJwt.Strategy(options, function (decodedJwt, next) {
-      const username = decodedJwt.username;
-      db.any("SELECT * FROM accounts ")
-        .then(function (data) {
-          const account = data.find((account) => account.name === username);
-          if (account !== undefined) {
-            next(null, account);
-          } else {
-            return next(null, false);
-          }
-        })
-        .catch(function (error) {
-          next(error, false);
-          console.error(error);
-        });
-    })
-  );
-app.use(passport.initialize());
