16875ebf0e5dec1906a164eb80b0b93b5fe44d4c
emilyroseschmidt <92035095+emilyroseschmidt@users.noreply.github.com>
2021-10-26 16:07:07 +0100

    Added error handling

--- src/backend/controllers/CameraController.js
+++ src/backend/controllers/CameraController.js
@@ -1,12 +1,25 @@
 import express from "express";
 import got from "got";
-import { apiKey } from "../config"
+import { apiKey } from "../config";
 const router = express.Router();
+import { NotFoundErrorName } from "../errors/NotFoundError.js";
 
 router.get("/:rover", (req, res) => {
     const roverName = req.params.rover;
+    const possibleNames = [
+        "curiosity",
+        "perseverance",
+        "spirit",
+        "opportunity",
+    ];
+
+    const resulting = possibleNames.find((item) => item === roverName);
+    if (resulting === undefined) {
+        res.status(404).send({ errors: "No rover with that name" });
+    }
+
     got.get(
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}?api_key=${apiKey}`
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/${resulting}?api_key=${apiKey}`
     )
         .json()
 
@@ -15,6 +28,15 @@ router.get("/:rover", (req, res) => {
                 return { name: item.name, fullName: item.full_name };
             });
             res.send(cameraMap);
+        })
+        .catch((err) => {
+            console.log(err);
+            if (err.name === NotFoundErrorName) {
+                res.sendStatus(404);
+            } else {
+                console.error(err);
+                res.sendStatus(500);
+            }
         });
 });
 
