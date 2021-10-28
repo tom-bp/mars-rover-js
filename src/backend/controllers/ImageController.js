bb32fc9cf9f18a16ef01c1b4305e92543792eb37
CallumKerr02 <callumkerr3@hotmail.com>
2021-10-28 12:22:51 +0100

    Updated, giving user errors depending on poor input.

--- src/backend/controllers/ImageController.js
+++ src/backend/controllers/ImageController.js
@@ -5,9 +5,37 @@ import { NotFoundErrorName } from "../errors/NotFoundError";
 const router = express.Router();
 
 router.get("/rovers/:rover/cameras/:camera/sol/:sol/", async (req, res) => {
+    const possibleNames = [
+        "curiosity",
+        "perseverance",
+        "spirit",
+        "opportunity",
+    ];
+    const possibleCameraNames = [
+        "FHAZ",
+        "RHAZ",
+        "MAST",
+        "CHEMCAM",
+        "MAHLI",
+        "MARDI",
+        "NAVCAM",
+        "PANCAM",
+        "MINITES",
+    ]
     const roverName = req.params.rover;
     const solDate = req.params.sol;
+    if (solDate > 3550){
+        res.status(404).send({ errors: "Invalid Sol Date" });
+    }
     const cameraName = req.params.camera;
+    const resultingName = possibleNames.find((item) => item === roverName);
+    if (resultingName === undefined) {
+        res.status(404).send({ errors: "No rover with that name" });
+    }
+    const resultingCameraName = possibleCameraNames.find((item) => item === cameraName);
+    if (resultingCameraName === undefined) {
+        res.status(404).send({ errors: "Name of camera does not exist" });
+    }
     got.get(
         `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${solDate}&camera=${cameraName}&api_key=${apiKey}`
     )
@@ -18,14 +46,6 @@ router.get("/rovers/:rover/cameras/:camera/sol/:sol/", async (req, res) => {
                 return item.img_src;     
             })
             res.send(imageLink);
-        })
-        .catch(function(error) {
-            if (error.roverName !== NotFoundErrorName){
-            res.sendStatus(404);
-            } else{
-                console.error(error);
-                res.sendStatus(500);
-            }
         })
 });
 
