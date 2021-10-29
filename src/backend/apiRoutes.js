e90b87914acab47f309e70db78bb71d95a3e6fba
Slurp5 <60278250+Slurp5@users.noreply.github.com>
2021-10-29 14:49:33 +0100

    Clean up

--- src/backend/apiRoutes.js
+++ src/backend/apiRoutes.js
@@ -3,7 +3,6 @@ import QuotationsController from "./controllers/QuotationsController.js";
 import ImageController from "./controllers/ImageController.js";
 import CameraController from "./controllers/CameraController.js";
 import loginController from "./controllers/loginController.js";
-import perseveranceLocation from "../frontend/pages/perseveranceLocation/PerseveranceLocation.js"
 
 const router = express.Router();
 
@@ -11,7 +10,6 @@ router.use("/quotations", QuotationsController);
 router.use("/pictures", ImageController);
 router.use("/cameras", CameraController);
 router.use("/login", loginController);
-router.use("/perseverance-location", perseveranceLocation)
 
 router.use((req, res) => {
     res.sendStatus(404);
